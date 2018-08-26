24bc5f9af8c60f989de48b8923e32c892198afce
Shibu Tamang <lamashibu19@gmail.com>
2018-08-26 09:09:40 +0545

    project uploading

---
+++ SignalR/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
